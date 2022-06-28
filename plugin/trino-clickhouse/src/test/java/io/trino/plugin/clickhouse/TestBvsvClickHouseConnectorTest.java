bfc97588f6e87055eeae586814d4303596f6dd64
sergey.boltanov <sergey.boltanov@gmail.com>
2022-06-28 21:27:08 +0400

    add date format test for ClickHouse

--- plugin/trino-clickhouse/src/test/java/io/trino/plugin/clickhouse/TestBvsvClickHouseConnectorTest.java
+++ plugin/trino-clickhouse/src/test/java/io/trino/plugin/clickhouse/TestBvsvClickHouseConnectorTest.java
@@ -9,9 +9,28 @@ public class TestBvsvClickHouseConnectorTest extends TestClickHouseConnectorTest
     @Test
     public void truncateSupportTest() {
 
-        String tableName = "test_drop_column_" + randomTableSuffix();
+        String tableName = "test_truncate_table_" + randomTableSuffix();
         assertUpdate("CREATE TABLE " + tableName + "(x int NOT NULL, y int, a int) WITH (engine = 'MergeTree', order_by = ARRAY['x'])");
         assertUpdate("INSERT INTO " + tableName + "(x,y,a) SELECT 123, 456, 111", 1);
         assertUpdate("TRUNCATE TABLE " + tableName);
+    }
+
+    @Test
+    public void dateTypeWriteAndReadTest() {
+        String tableName = "test_date_table_" + randomTableSuffix();
+
+        assertUpdate("CREATE TABLE " + tableName + "(orderdate date, orderstatus varchar) WITH (engine = 'LOG')");
+        assertUpdate("INSERT INTO " + tableName + " VALUES( date '2022-01-01', 'NEW')", 1);
+        assertQuery("SELECT orderdate FROM " + tableName, "VALUES(date '2022-01-01')");
+
+
+    }
+
+    @Test
+    public void dateTypeWriteAndReadTestFail() {
+        String tableName = "test_date_table_" + randomTableSuffix();
+
+        assertUpdate("CREATE TABLE " + tableName + "(orderdate date, orderstatus varchar) WITH (engine = 'LOG')");
+        assertQueryFails("INSERT INTO " + tableName + " VALUES( date '222-01-01', 'NEW')", "Date must be between 1970-01-01 and 2106-02-07 in ClickHouse: 0222-01-01");
     }
 }
